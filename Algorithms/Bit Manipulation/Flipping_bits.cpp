b422a413f08dcc71fc9815948299d7f2cbb9c83c
Aydin Simsek <aydn.simsek@gmail.com>
2021-01-19 01:37:38 +0300

    initial commit

---
+++ Algorithms/Bit Manipulation/Flipping_bits.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long flippingBits(long n) {
+    return pow(2, 32)-1-n;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        long n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        long result = flippingBits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
