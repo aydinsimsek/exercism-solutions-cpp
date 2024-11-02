f02a6d86ee49daaa253486b126760e131271c6a3
Aydın Şimşek <aydn.simsek@gmail.com>
2024-11-02 22:10:35 +0300

    added solution to Q219

---
+++ 219. Contains Duplicate II/solution.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution 
+{
+    public:
+        bool containsNearbyDuplicate(std::vector<int>& nums, int k) 
+        {
+            std::unordered_map <int, int> mp;
+            for(int i = 0; i < nums.size(); i++)
+            {
+                if(mp.find(nums[i]) != mp.end() && i - mp[nums[i]] <= k)
+                {
+                    return true;
+                }
+                mp[nums[i]] = i;
+            }
+            return false;
+        }
+};
