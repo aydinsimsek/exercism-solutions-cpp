9718847d2ed0d0997bf576a40627e7b580e45881
Aydın Şimşek <aydn.simsek@gmail.com>
2024-11-09 04:47:19 +0300

    added solution to Q42

---
+++ 42. Trapping Rain Water/solution.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+
+class Solution
+{
+    public:
+        int trap(std::vector<int>& height)
+        {
+            int res = 0;
+            int currIdx = 0;
+            while(currIdx < height.size()-1)
+            {
+                if(height[currIdx] == 0)
+                {
+                    currIdx++;
+                    continue;
+                }
+                int rightIdx = -1;
+                int currMax = 0;
+                for(int i = currIdx+1; i < height.size(); i++)
+                {   
+                    if(height[i] > currMax)
+                    {
+                        currMax = height[i];
+                        rightIdx = i;
+                        if(currMax > height[currIdx])
+                        {
+                            break;
+                        }
+                    }
+                }
+                if(rightIdx == -1)
+                {
+                    currIdx++;
+                    continue;
+                }
+                for(int i = currIdx+1; i < rightIdx; i++)
+                {
+                    res -= height[i];
+                }
+                int _height;
+                if(height[currIdx] < height[rightIdx])
+                {
+                    _height = height[currIdx];
+                }
+                else 
+                {
+                    _height = height[rightIdx];
+                }
+                int _width = rightIdx - currIdx - 1;
+                res += _height * _width;
+                currIdx = rightIdx;
+            }
+            return res;
+        }
+};
