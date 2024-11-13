fb0ee29922c19603502228024f0943d5fb71a9cb
Aydın Şimşek <aydn.simsek@gmail.com>
2024-11-14 00:36:13 +0300

    optimized solution to Q172 for O(log n) time complexity

--- 172. Factorial Trailing Zeroes/solution.cpp
+++ 172. Factorial Trailing Zeroes/solution.cpp
@@ -4,15 +4,9 @@ class Solution
         int trailingZeroes(int n)
         {
             int count = 0;
-            int tmp;
-            for(int i = 5; i <= n; i += 5)
+            for(int i = 5; i <= n; i *= 5)
             {
-                tmp = i;
-                while(tmp % 5 == 0)
-                {
-                    count++;
-                    tmp /= 5;
-                }
+                count += (n / i);
             }
             return count;
         }
